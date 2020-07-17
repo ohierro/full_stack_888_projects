43466056a58521a0675654f9404c24fc3621b232
Óliver <ohierro@gmail.com>
2020-07-17 11:33:27 -0400

    Added 15th day examples

---
+++ byDay/day15/src/com/hiberus/sort/ArraySort.java
@@ -0,0 +1,37 @@
+package com.hiberus.sort;
+
+import java.util.Random;
+
+public class ArraySort {
+    private int[] valores = new int[0];
+
+    public ArraySort(int size) {
+        valores = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = random.nextInt(100);
+        }
+    }
+
+    public void sort() {
+        for (int i = 0; i < valores.length; i++) {
+            sortSimple();
+        }
+    }
+
+    private void sortSimple() {
+        for (int j = 0; j < valores.length - 1; j++) {
+            if (valores[j + 1] < valores[j]) {
+                int aux = valores[j+1];
+                valores[j+1] = valores[j];
+                valores[j] = aux;
+            }
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < valores.length; i++) {
+            System.out.println(valores[i]);
+        }
+    }
+}
---
+++ byDay/day15/src/com/hiberus/sort/Main.java
@@ -0,0 +1,10 @@
+package com.hiberus.sort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArraySort instancia = new ArraySort(30);
+        instancia.sort();
+        instancia.print();
+    }
+}
